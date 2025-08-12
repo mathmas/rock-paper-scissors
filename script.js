ffafcdc48d846d0dd6a0979ffcee762d5eada8ad
SakolBien <sakolbien@desk.home>
2025-08-12 15:47:27 +0200

    feat: one round playable

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 // variables declarations
+const rockString = "rock";
+const paperString = "paper";
+const scissorsString = "scissors";
 
 let userScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoice());
-console.log(getUserChoice());
+playRound(getComputerChoice(), getUserChoice());
 
 //  functions declarations
+
+function playRound(computerChoice, userChoice){
+    userChoice = userChoice.toLowerCase();
+    console.log(userChoice);
+    console.log(computerChoice);
+
+
+    if(userChoice === computerChoice){    // Draw
+        console.log("It's a draw, no point.");
+    } else if(     // User Lose
+        (userChoice === rockString && computerChoice === paperString) ||
+        (userChoice === paperString && computerChoice === scissorsString) ||
+        (userChoice === scissorsString && computerChoice === rockString)
+    ){
+        console.log(`You lose! ${computerChoice} beats ${userChoice}.`);
+        computerScore++;
+    } else {    // User Win
+        console.log(`You win! ${userChoice} beats ${computerChoice}.`);
+        userScore++;
+    }
+}
 
 function getComputerChoice(){
     switch (Math.floor(Math.random()*3)) {
         case 0:
-            return "rock";
+            return rockString;
         case 1:
-            return "paper";
+            return paperString;
         case 2:
-            return "scissors";
+            return scissorsString;
         default:
             return "Error!";
     }
 }
 
 function getUserChoice(){
-    return prompt("rock, paper or scissors ?", "rock");
+    return prompt(`${rockString}, ${paperString} or ${scissorsString} ?`, rockString);
 }
