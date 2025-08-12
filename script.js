d0ca0e4b21c42ee84d33ab5728628491ca268597
SakolBien <sakolbien@desk.home>
2025-08-12 15:02:19 +0200

    feat: computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+console.log(getComputerChoice());
+
+function getComputerChoice(){
+    switch (Math.floor(Math.random()*3)) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "Error!";
+    }
+}
