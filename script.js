a4ac93ffe272914b79ec425caf66bf6699ae65a8
SakolBien <sakolbien@desk.home>
2025-09-02 16:06:40 +0200

    feat: ui using the DOM

--- script.js
+++ script.js
@@ -2,47 +2,55 @@
 const ROCK_STRING = "rock";
 const PAPER_STRING = "paper";
 const SCISSORS_STRING = "scissors";
+ 
+let userScore = 0; 
+let computerScore = 0; 
 
-//playGame();
 
 const container = document.querySelector(".container");
-container.addEventListener("click", (e) => console.log(getUserChoice(e.target)))
+const score = document.getElementById("score");
+const paraComputerChoice = document.getElementById("game-action")
+
+container.addEventListener("click", function(e){
+    const computChoice = getComputerChoice();
+    const userChoice = getUserChoice(e.target)
+    playRound(computChoice, userChoice)
+
+    score.textContent = `Score: ${userScore} : ${computerScore}`;
+    paraComputerChoice.textContent = `The computer made: ${computChoice}, and you made ${userChoice}`;
+
+    if(userScore >= 5) {
+        console.log(`You win! ${userScore} : ${computerScore}`);
+        container.style.display = "none";
+    }
+    if(computerScore >= 5) {
+        console.log(`You lose! ${userScore} : ${computerScore}`)
+        container.style.display = "none";
+    }
+
+    }
+)
+
 //  functions declarations
 
-function playGame(){
-    // variables declarations
-    let userScore = 0;
-    let computerScore = 0;
-
-    // Main
-
-    playRound(getComputerChoice(), getUserChoice());   
-    
-    if(userScore > computerScore) {console.log(`You win! ${userScore} : ${computerScore}`);}
-    else if(userScore < computerScore) {console.log(`You lose! ${userScore} : ${computerScore}`);}
-    else {console.log(`It's a draw! ${userScore} : ${computerScore}`)}
-
-    //  functions declarations
-
-    function playRound(computerChoice, userChoice){
-        userChoice = userChoice.toLowerCase();
-        console.log(userChoice);
-        console.log(computerChoice);
+function playRound(computerChoice, userChoice){
+    userChoice = userChoice.toLowerCase();
+    console.log(userChoice);
+    console.log(computerChoice);
 
 
-        if(userChoice === computerChoice){    // Draw
-            console.log("It's a draw, no point.");
-        } else if(     // User Lose
-            (userChoice === ROCK_STRING && computerChoice === PAPER_STRING) ||
-            (userChoice === PAPER_STRING && computerChoice === SCISSORS_STRING) ||
-            (userChoice === SCISSORS_STRING && computerChoice === ROCK_STRING)
-        ){
-            console.log(`You lose! ${computerChoice} beats ${userChoice}.`);
-            computerScore++;
-        } else {    // User Win
-            console.log(`You win! ${userChoice} beats ${computerChoice}.`);
-            userScore++;
-        }
+    if(userChoice === computerChoice){    // Draw
+        console.log("It's a draw, no point.");
+    } else if(     // User Lose
+        (userChoice === ROCK_STRING && computerChoice === PAPER_STRING) ||
+        (userChoice === PAPER_STRING && computerChoice === SCISSORS_STRING) ||
+        (userChoice === SCISSORS_STRING && computerChoice === ROCK_STRING)
+    ){
+        console.log(`You lose! ${computerChoice} beats ${userChoice}.`);
+        computerScore++;
+    } else {    // User Win
+        console.log(`You win! ${userChoice} beats ${computerChoice}.`);
+        userScore++;
     }
 }
 
